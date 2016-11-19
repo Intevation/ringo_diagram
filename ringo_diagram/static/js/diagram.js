85eb2efef69c8af87c42b2b2b1df220631b69d08
Torsten Irländer <torsten.irlaender@googlemail.com>
2016-11-19 18:35:53 +0100

    Make separate legend configureable

--- ringo_diagram/static/js/diagram.js
+++ ringo_diagram/static/js/diagram.js
@@ -1,15 +1,21 @@
 // Used to synchronize the diagrams
 var diagram_syncs = [];
 
-function renderDiagram(id, data, title, xlabel, ylabel, fieldname, errorBars, hlcb, uhlcb) {
+function renderDiagram(id, data, title, xlabel, ylabel, fieldname, errorBars, legend, hlcb, uhlcb) {
+  if (legend === true) {
+      legend = fieldname+'_legend'
+  } else {
+      legend = null
+  }
   g = new Dygraph(document.getElementById(id), data, {
     legend: 'always',
     title: title,
     strokeWidth: 1,
     highlightCircleSize: 5,
+    axisLabelFontSize: 11,
     xlabel: xlabel,
     ylabel: ylabel,
-    labelsDiv: fieldname+'_legend',
+    labelsDiv: legend,
     labelsSeparateLines: true,
     errorBars: errorBars,
     highlightCallback: hlcb,
